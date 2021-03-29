a12f3a741f5ab9225c0621964a0055e83352d99a
lulu <l1990790120@gmail.com>
2021-03-29 19:07:19 -0400

    [change] use cobra as cmd interface base

---
+++ cmd/pad/commands/root.go
@@ -0,0 +1,32 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var root = &cobra.Command{
+	Use:   "pad",
+	Short: "Pad is generates templated email html",
+	Long:  `Use Post-it Pad to generate templated email html with specific layout or as a development kit to create new email layout. Check out documentation is at https://apiobuild.com/docs/.`,
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+var version = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of Pad",
+	Run: func(cmd *cobra.Command, args []string) {
+		// TODO: update
+		fmt.Println("Hugo Static Site Generator v0.9 -- HEAD")
+	},
+}
+
+func Execute() {
+	root.AddCommand(version)
+	if err := root.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ cmd/pad/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/apiobuild/post-it-pad/cmd/pad/commands"
+)
+
+func main() {
+	commands.Execute()
+}
